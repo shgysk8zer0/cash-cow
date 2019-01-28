c2bcf26efbaee976287d7554a9be56b52bef2852
Chris Zuber <shgysk8zer0@gmail.com>
2019-01-28 00:32:26 -0800

    Add login & registration

---
+++ components/registration-form/registration-form.js
@@ -0,0 +1,65 @@
+import {importLink} from '../../js/std-js/functions.js';
+
+export default class HTMLRegistrationFormElement extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({mode: 'open'});
+		importLink('login-form-template').then(async tmp => {
+			tmp = tmp.cloneNode(true);
+			const container = document.createElement('div');
+			container.append(...tmp.head.children, ...tmp.body.children);
+			this.shadowRoot.append(container);
+			const form = this.form;
+			container.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
+
+			form.addEventListener('submit', async event => {
+				event.preventDefault();
+				const body = new FormData(this.form);
+				const headers = new Headers({Accept: 'application/json'});
+				const url = new URL('http://localhost:8000/login/');
+				const resp = await fetch(url, {
+					method: 'POST',
+					mode: 'cors',
+					body,
+					headers,
+				});
+
+				if (resp.ok) {
+					const detail = await resp.json();
+					this.form.reset();
+					this.dialog.close();
+					document.dispatchEvent(new CustomEvent('login',{detail}));
+				} else {
+					console.error(`${resp.url} [${resp.status} ${resp.statusText}]`);
+				}
+			});
+			form.addEventListener('reset', () => container.querySelector('dialog').close());
+			this.dispatchEvent(new Event('ready'));
+		});
+	}
+
+	get dialog() {
+		if (this.shadowRoot.childElementCount === 0) {
+			throw new Error('Login form not yet ready');
+		} else {
+			return this.shadowRoot.querySelector('dialog');
+		}
+	}
+
+	get form() {
+		return this.dialog.querySelector('form');
+	}
+
+	async ready() {
+		if (this.shadowRoot.childElementCount === 0) {
+			await new Promise(resolve => this.addEventListener('ready', () => resolve(), {once: true}));
+		}
+	}
+
+	async register() {
+		await this.ready();
+		this.dialog.showModal();
+	}
+}
+
+customElements.define('registration-form', HTMLRegistrationFormElement);
--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ import './std-js/deprefixer.js';
 import './std-js/shims.js';
 import '../components/current-year.js';
 import '../components/share-button.js';
+import '../components/login-form/login-form.js';
+import '../components/registration-form/registration-form.js';
 
 document.documentElement.classList.replace('no-js', 'js');
 document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
