3114392892817ec5a018819400ce1bb77581b978
Chris Zuber <shgysk8zer0@gmail.com>
2019-02-01 23:08:26 -0800

    Misc. changes & enhancements

--- js/index.js
+++ js/index.js
@@ -5,13 +5,15 @@ import 'https://cdn.chriszuber.com/components/share-button.js';
 import 'https://cdn.chriszuber.com/components/login-button.js';
 import 'https://cdn.chriszuber.com/components/register-button.js';
 import 'https://cdn.chriszuber.com/components/logout-button.js';
-import 'https://cdn.chriszuber.com/components/gravatar-img.js';
+import 'https://cdn.chriszuber.com/components/imgur-img.js';
+import 'https://cdn.chriszuber.com/components/imgur-gallery/imgur-gallery.js';
 import 'https://cdn.chriszuber.com/components/login-form/login-form.js';
 import 'https://cdn.chriszuber.com/components/registration-form/registration-form.js';
 import {registerServiceWorker} from 'https://cdn.chriszuber.com/js/std-js/functions.js';
 
 document.documentElement.classList.replace('no-js', 'js');
 document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
+
 if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
 	registerServiceWorker(document.documentElement.dataset.serviceWorker).catch(console.error);
 }
